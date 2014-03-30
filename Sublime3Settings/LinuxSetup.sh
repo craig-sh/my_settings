bc03e3b26b90a4aa1dcb97474fba15de7e847b18
craig-sh <craig.henriques@mail.utoronto.com>
2014-03-29 23:04:26 -0700

    Only Backing up crucial sublime files

--- Sublime3Settings/LinuxSetup.sh
+++ Sublime3Settings/LinuxSetup.sh
@@ -1,5 +1,7 @@
 cd ~/.config/sublime-text-3
-mv Installed\ Packages Installed\ Packages\ Back
-mv Packages Packages\ Back
-ln -s ~/my_settings/Sublime3Settings/Packages ./Packages
-ln -s ~/my_settings/Sublime3Settings/Installed\ Packages ./Installed\ Packages
+#mv Installed\ Packages Installed\ Packages\ Back
+
+mv Packages/User User_back
+ln -s ~/my_settings/Sublime3Settings/Packages/User ./Packages/User
+
+#ln -s ~/my_settings/Sublime3Settings/Installed\ Packages ./Installed\ Packages
