390755875430dd67890efce566495c3ed6409df6
craig-sh <craig.s.henriques@gmail.com>
2016-08-21 09:08:02 -0400

    Update settings for gruvbox

--- setup.sh
+++ setup.sh
@@ -38,7 +38,8 @@ git clone git://github.com/scrooloose/syntastic.git
 git clone git://github.com/Lokaltog/vim-easymotion.git
 git clone git://github.com/Lokaltog/vim-powerline.git
 git clone git://github.com/scrooloose/nerdtree.git
-git clone git://github.com/altercation/vim-colors-solarized.git
+#git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/morhetz/gruvbox.git
 git clone git://github.com/tpope/vim-fugitive.git
 git clone git://github.com/tpope/vim-sleuth.git
 
