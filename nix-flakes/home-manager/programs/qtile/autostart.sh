f0b9e262501ad0eec4ea7027440e2b3b572600a8
craig-sh <craig.s.henriques@gmail.com>
2022-07-15 23:30:46 -0400

    Remove autostart  programs

--- nix-flakes/home-manager/programs/qtile/autostart.sh
+++ nix-flakes/home-manager/programs/qtile/autostart.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-xss-lock /usr/local/bin/locksreen.sh &
+#xss-lock /usr/local/bin/locksreen.sh &
 dunst &
-<<<<<<< Updated upstream
-/usr/lib/kdeconnectd &
-xautolock -time 15 -locker '/usr/local/bin/lock' &
-=======
 keepassxc &
-#/usr/lib/kdeconnectd &
-#xautolock -time 15 -locker '/usr/local/bin/lock' &
->>>>>>> Stashed changes
