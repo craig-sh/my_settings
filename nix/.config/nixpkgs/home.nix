015b8db68974cfc1cb893b1e1541d229cb82aaf2
craig-sh <craig.s.henriques@gmail.com>
2023-01-14 21:38:22 -0500

    NIX: Add starship

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -25,6 +25,35 @@
   home.file.".zshenv".source = ../../../zsh/.zshenv;
   home.file.".zshrc".source = ../../../zsh/.zshrc;
 
+  programs.starship = {
+      enable = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+        # add_newline = false;
+
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+
+        directory = {
+          truncation_length = 0;
+        };
+
+        git_status = {
+          conflicted = "🏳";
+          ahead = "🏎💨";
+          behind = "😰";
+          diverged = "😵";
+          untracked = "🤷";
+          stashed = "📦";
+          modified = "📝";
+          staged = ''[++\($count\)](green)'';
+          renamed = "👅";
+          deleted = "🗑";
+        };
+      };
+    };
 
   programs.git = {
     enable = true;
