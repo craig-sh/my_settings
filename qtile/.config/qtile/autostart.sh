9e943660b9773679539bc792f85d07dfdbcd15c9
craig-sh <craig.s.henriques@gmail.com>
2023-07-13 22:27:48 -0400

    Add keepass to autostart

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -2,5 +2,11 @@
 
 xss-lock /usr/local/bin/locksreen.sh &
 dunst &
+<<<<<<< Updated upstream
 /usr/lib/kdeconnectd &
 xautolock -time 15 -locker '/usr/local/bin/lock' &
+=======
+keepassxc &
+#/usr/lib/kdeconnectd &
+#xautolock -time 15 -locker '/usr/local/bin/lock' &
+>>>>>>> Stashed changes
