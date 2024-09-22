464b549f5bc998bf59e398ee6ce0bb5848113078
craig-sh <craig.s.henriques@gmail.com>
2024-09-22 08:41:50 -0400

    Bump homelab versions

--- nix-flakes/nixos/virt-base-configuration.nix
+++ nix-flakes/nixos/virt-base-configuration.nix
@@ -78,7 +78,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    k3s
+    k3s_1_29
     nfs-utils
     age
     sops
