94718c3ac0ac11e14b787a6a9462494f7c4b56df
craig-sh <craig.s.henriques@gmail.com>
2024-10-20 21:06:13 -0400

    Fix neovim snippets/injections not working

--- nix-flakes/home-manager/programs/neovim.nix
+++ nix-flakes/home-manager/programs/neovim.nix
@@ -14,6 +14,10 @@
 
   home.file = {
     ".config/nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/craig/my_settings/neovim/.config/nvim/init.lua";
+    #".config/nvim/after/ftplugin/org.lua".source = config.lib.file.mkOutOfStoreSymlink "/home/craig/my_settings/neovim/.config/nvim/after/ftplugin/org.lua";
+    #".config/nvim/after/ftplugin/qf.vim".source = config.lib.file.mkOutOfStoreSymlink "/home/craig/my_settings/neovim/.config/nvim/after/ftplugin/qf.vim";
+    ".config/nvim/after".source = config.lib.file.mkOutOfStoreSymlink "/home/craig/my_settings/neovim/.config/nvim/after";
+    ".config/nvim/mysnips".source = config.lib.file.mkOutOfStoreSymlink "/home/craig/my_settings/neovim/.config/nvim/mysnips";
     ".config/nvim/lua/tools.lua".text = ''
       return {
             gcc = '${lib.getExe pkgs.gcc}';
