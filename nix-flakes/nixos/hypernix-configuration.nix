1327b6f350dc4ae785a09bbeb1062735e6de069c
craig-sh <craig.s.henriques@gmail.com>
2024-11-04 17:53:34 -0500

    Start VR for hypernix (not fully working yet)

--- nix-flakes/nixos/hypernix-configuration.nix
+++ nix-flakes/nixos/hypernix-configuration.nix
@@ -42,4 +42,16 @@
       Option "VariableRefresh" "true"
       Option "AsyncFlipSecondaries" "true"
   '';
+
+  # VR https://wiki.nixos.org/wiki/VR
+  services.monado = {
+    enable = true;
+    defaultRuntime = true; # Register as default OpenXR runtime
+  };
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+    WMR_HANDTRACKING = "0"; # disable handtrackiong for now
+  };
+
 }
