a5a6519ed2c70f3d8a2e4b22f8f57656851aff10
craig-sh <craig.s.henriques@gmail.com>
2025-09-30 21:32:12 -0400

    NVIM: add testing plugin

--- neovim/.config/nvim/lua/plugins/code_utility.lua
+++ neovim/.config/nvim/lua/plugins/code_utility.lua
@@ -42,5 +42,25 @@ return {
         desc = "Quickfix List (Trouble)",
       },
     },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({
+            runner = "pytest",
+            dap = { justMyCode = true },
+          }),
+        },
+      })
+    end,
   }
 }
--- neovim/.config/nvim/lua/plugins/utility.lua
+++ neovim/.config/nvim/lua/plugins/utility.lua
@@ -53,6 +53,7 @@ return {
     "rachartier/tiny-glimmer.nvim",
     event = "VeryLazy",
     opts = {},
+    priority = 10, -- Needs to be a really low priority, to catch others plugins keybindings.
   },
   {
     'mikesmithgh/kitty-scrollback.nvim',
