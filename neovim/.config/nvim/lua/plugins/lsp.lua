65fbdafe28ba4e123253612fa967f803f7affe2e
craig-sh <craig.s.henriques@gmail.com>
2025-12-18 22:35:37 -0500

    Swtich form basedpyright to ty

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -69,7 +69,7 @@ return {
       end
 
 
-      local servers = { "hls", "basedpyright", "rust_analyzer", "bashls", "ansiblels", "nixd", "ruff" }
+      local servers = { "hls", "ty", "rust_analyzer", "bashls", "ansiblels", "nixd", "ruff" }
       for _, lsp in ipairs(servers) do
         vim.lsp.enable(lsp)
         vim.lsp.config(lsp, {
