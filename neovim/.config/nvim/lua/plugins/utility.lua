5f48a1c46016c4dd9848a415aa0eab0acc3f9eff
craig-sh <craig.s.henriques@gmail.com>
2026-01-06 21:37:29 -0500

    NVIM: Fix mapping bug

--- neovim/.config/nvim/lua/plugins/utility.lua
+++ neovim/.config/nvim/lua/plugins/utility.lua
@@ -37,7 +37,8 @@ return {
 
       -- Use the traversal keys to repeat the previous motion without
       -- explicitly invoking Leap:
-      require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+      --
+      --require('leap.user').set_repeat_keys('<enter>', '<backspace>')
 
 
       vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
