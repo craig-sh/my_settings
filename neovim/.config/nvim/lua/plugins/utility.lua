b7d02057598ef985cdf48cd5c35a24eeb704c491
craig-sh <craig.s.henriques@gmail.com>
2026-02-01 20:08:02 -0500

    Fix image.nvim breaking

--- neovim/.config/nvim/lua/plugins/utility.lua
+++ neovim/.config/nvim/lua/plugins/utility.lua
@@ -5,9 +5,9 @@ return {
   {
       "3rd/image.nvim",
       build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-      opts = {
-          processor = "magick_rock",
-      }
+      --opts = {
+      --    processor = "magick_rock",
+      --}
   },
   { 'tpope/vim-sleuth' }, --  Indentation settings
   { 'norcalli/nvim-colorizer.lua', opts = {} },
